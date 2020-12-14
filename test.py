b074e23abf03343d967a08dfdc700af9595ff58d
panjacob <s19615@pjwstk.edu.pl>
2020-12-14 18:12:31 +0100

    DES - complete, main

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+from des_algorithm import Des1, Des3
+
+
+class TestAlgorithms(unittest.TestCase):
+
+    def test_1des(self):
+        original_message = 'Testujemy 1des!'
+        des1 = Des1('some key')
+        encrypted = des1.encrypt(original_message)
+        decrypted = des1.decrypt(encrypted)
+
+        self.assertEqual(decrypted, original_message)
+
+    def test_3des(self):
+        original_message = 'Testujemy 3des!'
+        des3 = Des3('a key for TRIPLE')
+        encrypted = des3.encrypt(original_message)
+        decrypted = des3.decrypt(encrypted)
+
+        self.assertEqual(decrypted, original_message)
+
+
+if __name__ == '__main__':
+    unittest.main()
